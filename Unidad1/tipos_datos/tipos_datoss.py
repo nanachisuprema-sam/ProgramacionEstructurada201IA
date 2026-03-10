d66a03e3ba8614f34f023caf77490d380c8e0176
Sam <nanachisuprema@gmail.com>
2026-03-10 08:31:43 -0600

    Tipos_datos

---
+++ Unidad1/tipos_datos/tipos_datoss.py
@@ -0,0 +1,28 @@
+#demostración tipoa de datos python
+
+def datos():
+    entero= 25
+    decimal=3,14
+    cadena= "hola mundo!"
+    booleano= True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(booleano)
+
+def tipos_datos_compuestos():
+    lista= (10,20,30,40)
+    tupla= (19,29,39,49)
+    diccionario= {"nombre": "Juan", "edad": 30, "ciudad": "Madrid"}
+    
+    print(lista)
+    print(tupla)
+    print(diccionario)
+
+def main():
+    datos()
+    tipos_datos_compuestos()
+
+if __name__=="__main__":
+    main()
