c18c53bdd3c144e13e3185017ff8421d864c1747
Sam <nanachisuprema@gmail.com>
2026-03-13 08:17:49 -0600

    cadenas ejercicio

---
+++ Unidad1/ejercicio_cadenas/ejercicio_cadenas.py
@@ -0,0 +1,15 @@
+# ejercicio de cadenas
+
+
+def demostración_cadenas ():
+    cadena = "universidad de xalapa"
+    print(cadena.upper)
+    print(cadena.lower())
+    print(cadena.capitalize())
+    print(cadena.title())
+
+def main():
+    demostración_cadenas()
+
+if __name__=="__main__":
+    main()
