da0f0aa70e2536e0ed95a4e399ead5bdece7aa70
Sam <nanachisuprema@gmail.com>
2026-03-17 07:36:09 -0600

    ejercicios_aritmetica

---
+++ Unidad1/ejercicios_aritmetica/ejercicios_aritmeticos.py
@@ -0,0 +1,37 @@
+
+def resultados ():
+    suma= 10 + 4
+    print("suma= ", suma)
+    resta= 10-4
+    print("resta=", resta)
+    multi= 6*7
+    print("multiplicacion=",multi)
+    divicion_entera= 10//3
+    print("division=", divicion_entera)
+    modulo= 10 % 3
+    print("modulo=", modulo)
+    potenciacion= 2**3
+    print("potenciador=", potenciacion)
+
+def main():
+    resultados()
+if __name__=="__main__":
+    main()
+
+    """""
+     #ejemplo de uso de operadores
+    #suma
+    suma= 10 + 4
+    #resta
+    resta= 10-4
+    #multiplicacion
+    multi= 6*7
+    #divi entera
+    divicion_entera= 10//3
+    #
+    modulo= 10 % 3
+
+    #potenciación
+    potenciacion= 2**3
+
+    """""
