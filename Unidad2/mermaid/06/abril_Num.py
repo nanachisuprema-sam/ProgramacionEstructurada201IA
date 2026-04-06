b431affb8224f3ad3eac864b8021caef46932701
Sam <nanachisuprema@gmail.com>
2026-04-06 08:48:00 -0600

    numeros_impares

---
+++ Unidad2/mermaid/06/abril_Num.py
@@ -0,0 +1,14 @@
+
+N = int(input("Ingresa la cantidad de números impares: "))
+# Leer N
+N = int(input("Ingresa la cantidad de números impares: "))
+
+
+contador = 0
+numero = 1
+
+
+while contador < N:
+    print(numero)
+    numero = numero + 2
+    contador = contador + 1
