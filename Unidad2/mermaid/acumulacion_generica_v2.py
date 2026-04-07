e0be16caba084b515619cc721b9ac73dfee124a4
Sam <nanachisuprema@gmail.com>
2026-04-07 07:39:46 -0600

    Acumulación_v2

---
+++ Unidad2/mermaid/acumulacion_generica_v2.py
@@ -0,0 +1,17 @@
+def acumulacion():
+    suma=0
+    while True:
+        numero=int(input("ingrese numero"))
+        if numero>=10 and numero<=50:
+            suma+= numero
+        else:
+            break
+        suma+=numero
+    return suma
+
+def main():
+    resultado= acumulacion()
+    print("la suma acumulada es:", resultado)
+
+if __name__=="__main__":
+    main()
