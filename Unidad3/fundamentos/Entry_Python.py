f34cc3d2335ec0623a69d34c621628c3319b7359
Sam <nanachisuprema@gmail.com>
2026-04-20 07:54:38 -0600

    ejercicios_pthon

---
+++ Unidad3/fundamentos/Entry_Python.py
@@ -0,0 +1,45 @@
+def pregunta_1():
+    print(2**3**2)
+def pregunta_2():
+    x=10/2
+    print(type(x))
+def pregunta_3():
+    x=1 
+    x=x 
+    print(x==x)
+def pregunta_4():
+    print(1//2 *3)
+
+def pregunta_5():
+    y=2+3 *5
+    print(y)
+def pregunta_6():
+    b='2'
+    a='1'
+    print(a+b)
+def pregunta_7():
+    z=11 % 3
+    print(z)
+def pregunta_8():
+    x=5
+    y=2
+    print(x//y)
+def pregunta_9():
+    vol=10
+    vol+=5*2
+    print(vol)
+def pregunta_10():
+    print(bool(""), bool(""), bool(0), bool(0.00))
+def main():
+    pregunta_1()
+    pregunta_2()
+    pregunta_3()
+    pregunta_4()
+    pregunta_5()
+    pregunta_6()
+    pregunta_7()
+    pregunta_8()
+    pregunta_9()
+    pregunta_10()
+if __name__=="__main__":
+    main()
