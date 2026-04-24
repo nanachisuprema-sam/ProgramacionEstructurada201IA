c09cd67bc8c8f8e7141d13042f1e35f4ed625af8
Sam <nanachisuprema@gmail.com>
2026-04-24 08:48:11 -0600

    código bas agente discord

---
+++ Unidad3/discord/practica01/agente_logica.py
@@ -0,0 +1,15 @@
+"""
+# Tipos de Datos
+"int": "Representa números enteros (ej. 5, -10, 0). No tienen parte decimal.",
+
+# Funciones y Modularidad
+"def": "Es la palabra reservada para definir una función en Python.",
+
+# Operadores y Sintaxis
+"print": "Función que muestra información en la consola o salida estándar.",
+
+# 3. Lógica de búsqueda
+for clave in conocimiento:
+    if clave in mensaje:
+        return conocimiento[clave]
+"""
