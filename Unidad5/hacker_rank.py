2cd73e2cbd46349840cd5c184c8096078472a6ab
nanachisuprema-sam <nanachisuprema@gmail.com>
2026-05-26 07:57:19 -0600

    Escribe un número y detecta si es impar o par y cumple ciertos requisitos

---
+++ Unidad5/hacker_rank.py
@@ -0,0 +1,10 @@
+n=input("Escribe un numero nnmssss")
+num=int(n)
+if num % 2==0 and 2<= num  <= 5:
+    print("Not Weird")
+elif num % 2==0 and 6<= num  <=20:
+    print("Weird")
+elif num % 2==0 and num>30:
+    print("Not Weird")
+else: 
+    print("Weird")
