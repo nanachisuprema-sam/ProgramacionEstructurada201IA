c71cc823649de199ffd4c5e34fceececb0b77c60
nanachisuprema-sam <nanachisuprema@gmail.com>
2026-05-26 08:06:06 -0600

    correción

--- Unidad5/hacker_rank.py
+++ Unidad5/hacker_rank.py
@@ -4,7 +4,7 @@ if num % 2==0 and 2<= num  <= 5:
     print("Not Weird")
 elif num % 2==0 and 6<= num  <=20:
     print("Weird")
-elif num % 2==0 and num>30:
+elif num % 2==0 and num>20: #aca
     print("Not Weird")
 else: 
     print("Weird")
